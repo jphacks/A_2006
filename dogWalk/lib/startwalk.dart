eacdbecb2a480771a08527e5332a386f666c50b3
Yuta Tamura <tamura.go.tamura@gmail.com>
2020-11-05 22:12:52 +0900

    create: いろいろ

---
+++ dogWalk/lib/startwalk.dart
@@ -0,0 +1,217 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'dart:math' show cos, sqrt, asin;
+import 'dart:async';
+
+import './map.dart';
+import './main.dart';
+
+class StartWalk extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Maps',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home:WalkView(dstorage: DognameStorage(), cstorage: CounterStorage(),pstorage: PicStorage(), wstorage: WeightsStorage() ),
+    );
+  }
+}
+
+class WalkView extends StatefulWidget {
+  final CounterStorage cstorage; //初期設定が終わっているか
+  final DognameStorage dstorage; //犬の名前を扱う
+  final PicStorage pstorage; //写真を扱う
+  final WeightsStorage wstorage; //体重を保管する
+
+  WalkView({Key key, @required this.cstorage, @required this.dstorage, @required this.pstorage, @required this.wstorage}) : super(key: key);
+
+  @override
+  _WalkViewState createState() => _WalkViewState();
+}
+
+class _WalkViewState extends State<WalkView> {
+
+  final nameTextController = TextEditingController();
+  String dogWeight = "";
+  String allWeight;
+  String _name;
+  File file;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.dstorage.readName().then((String x){
+       setState((){
+          _name = x;
+       });
+       });
+  }
+
+
+  Widget _textField({
+    TextEditingController controller,
+    String label,
+    String hint,
+    double width,
+    Icon prefixIcon,
+    Widget suffixIcon,
+    Function(String) locationCallback,
+  }) {
+    return Container(
+      width: width * 0.8,
+      child: TextField(
+        onChanged: (value) {
+          locationCallback(value);
+        },
+        controller: controller,
+        decoration: new InputDecoration(
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          labelText: label,
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey[400],
+              width: 2,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            borderSide: BorderSide(
+              color: Colors.blue[300],
+              width: 2,
+            ),
+          ),
+          contentPadding: EdgeInsets.all(15),
+          hintText: hint,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomPadding:false,
+       appBar: AppBar(
+        title: Text('散歩しよう！',style: TextStyle(
+                                                      fontSize: 30,
+                                                      color: Colors.black87
+                                                      ),),
+      ),
+      body: SafeArea(
+
+              child: Column(
+                children: <Widget>[
+                  Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepOrange[50],
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                    width: width * 0.9,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 25.0, bottom: 25.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          SizedBox(height: 10),
+                          Text(
+                            'こんにちは, $_name さん',
+                            style: TextStyle(fontSize: 25.0),
+                          ),
+
+                          SizedBox(height: 10),
+
+                          _textField(
+                              label: '現在の体重',
+                              hint: '50',
+                              prefixIcon: Icon(Icons.looks_one_outlined),
+                              controller: nameTextController,
+                              width: width,
+                              locationCallback: (String value) {
+                                setState(() {
+                                  dogWeight = value;
+                                });
+                              }),
+                          SizedBox(height: 10),
+
+                          RaisedButton(
+                            onPressed: (dogWeight != '')
+                                ? () async {
+                                    widget.pstorage.readPic().then((File i){
+                                      setState((){
+                                        file = i;
+                                      });
+                                    }
+                                    );
+                                    widget.wstorage.readWeights().then((String i){
+                                      setState((){
+                                        allWeight = i + "," + dogWeight;
+                                      });
+                                    }
+                                    );
+                                    widget.wstorage.writeWeights(allWeight);
+                                    widget.wstorage.readWeights().then((String i){
+                                      print(i);
+                                    });
+                                    Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => MyMap(),)
+                                          );
+                                  }
+                                : null,
+                            color: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'ルート探索',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+                ]
+              )
+            ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
